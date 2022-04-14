78f3d44b8588de03bbac7964c890409c6e7bac42
nizzal <nizzal.gurung20@gmail.com>
2022-04-14 21:36:38 +0545

    added error handling

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # The Unix Workbench Project
-Date and time of makefile run: Tue Apr 12 06:35:47 +0545 2022
+Date and time of makefile run: Thu Apr 14 21:35:59 +0545 2022
 
-Number of lines of code: 24
+Number of lines of code: 35
 
 [GitHub Page](https://nizzal.github.io/unix-workbench-project)
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,35 @@
 #!/usr/bin/env bash
 
+re="^[0-9]+$"
+
 # get the number of files in the current directory
-num_of_files=$(find . -maxdepth 1 -type f | wc -l)
+num_of_files=$(ls | wc -l)
 
 echo "How many files are there in the current directory? "
-	
-# runs until user guesses the number 
-while [[ $user_input != $num_of_files ]]
-do
-	read user_input
 
-	if [[ user_input -lt num_of_files ]]
-	then
-		echo "too low"
-	elif [[ user_input -gt num_of_files ]]
-	then
-		echo "too high"
-	elif [[ num_of_files -eq user_input ]]
-	then
-		echo "Congratulations you guessed it right!"
-		exit
-	fi
-done
+function run_guess_program {	
+	# runs until user guesses the number 
+	while [[ $user_input != $num_of_files ]]
+	do
+		read user_input
+		# checking if the user input is number or not
+		if ! [[ $user_input =~ $re ]]
+		then
+			echo "Invalid input Please enter a valid input"
+		else
+			if [[ user_input -lt num_of_files ]]
+			then
+				echo "too low"
+			elif [[ user_input -gt num_of_files ]]
+			then
+				echo "too high"
+			elif [[ num_of_files -eq user_input ]]
+			then
+				echo "Congratulations you guessed it right!"
+				exit
+			fi
+		fi
+	done
+}
+
+run_guess_program
